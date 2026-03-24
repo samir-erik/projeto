041b61f7a00ac26f52d9ac1baa6aa6b3c8c2addc
ERIK SAMIR <SAMIRERIK826@GMAIL.COM>
2026-03-24 09:38:15 -0300

    Conectando o frontend com a API do Render

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log("JS CARREGOU");
 let todasNoticias = [];
 
+// URL base da sua API no Render
+const API_URL = "https://api-noticias-s2f9.onrender.com";
+
 async function carregarNoticias() {
-    const res = await fetch("http://127.0.0.1:5000/noticias");
+    const res = await fetch(`${API_URL}/noticias`);
     const dados = await res.json();
 
     todasNoticias = dados; // 🔥 salva tudo
@@ -10,7 +13,7 @@ async function carregarNoticias() {
 }
 
 async function filtrar(categoria) {
-    const res = await fetch(`http://127.0.0.1:5000/categoria/${categoria}`);
+    const res = await fetch(`${API_URL}/categoria/${categoria}`);
     const dados = await res.json();
 
     todasNoticias = dados; // 🔥 importante
@@ -40,7 +43,7 @@ async function buscar() {
 
     // Só busca se o usuário digitar pelo menos 3 letras
     if (termo.length >= 3) {
-        const res = await fetch(`http://127.0.0.1:5000/buscar/${termo}`);
+        const res = await fetch(`${API_URL}/buscar/${termo}`);
         const dados = await res.json();
         mostrar(dados);
     } 
